4d20be10d132b576f8b1676c45a1fe422cfc3ca4
Antoine-Bizet <bizet.antoine.pro@gmail.com>
2025-11-04 07:53:22 +0000

    Initial commit: Environnement de test JS prêt

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Laboratoire prêt !");
+
+
+const nom = "BIZET";
+let age = 17;
+console.log(nom);
+console.log(age);
+
+
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "secret123";
+
+if (motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
