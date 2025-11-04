adfaefa85b3b5a49f117c0fa6199ea64c595f773
Antoine-Bizet <bizet.antoine.pro@gmail.com>
2025-11-04 08:42:19 +0000

    Initial commit: Environnement de test JS prêt

--- script.js
+++ script.js
@@ -27,3 +27,21 @@ const moi = {
     competences: ["HTML", "CSS"]
 };
 console.log("je m'appelle" + " " + moi.prenom + " " + moi.nom + "," + " " + "j'ai" + " " + moi.age + " " + "et j'habite à" + " " + moi.ville)
+
+
+const notes = [12, 15, 9, 18];
+console.log(notes[0]);
+console.log(notes.length);
+for (let i = 0; i < notes.length; i++) {
+    console.log("La note à l'indice " + i + " est " + notes[i]);
+}
+
+const classe = [
+    { prenom: 'Alice', age: 18 },
+    { prenom: 'Bob', age: 17 }
+];
+
+for (let i = 0; i < classe.length; i++) {
+    if (classe[i].age >= 18)
+console.log(classe[i].prenom);
+}
