b3531a4d9e5f1769018c378c53e0be35ead01899
Antoine-Bizet <bizet.antoine.pro@gmail.com>
2025-11-04 08:17:18 +0000

    Initial commit: Environnement de test JS prêt

--- script.js
+++ script.js
@@ -12,8 +12,18 @@ let motDePasseUtilisateur = "secret123";
 
 if (motDePasseUtilisateur === motDePasseAttendu) {
     console.log("Accès autorisé. Bienvenue !");
-} else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
+} else if (motDePasseUtilisateur.length<=8) {
     console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
 } else {
     console.log("Accès refusé. Mot de passe incorrect.");
 }
+
+
+const moi = {
+    prenom: "Antoine",
+    nom: "Bizet",
+    age: 19,
+    ville: "Rillieux",
+    competences: ["HTML", "CSS"]
+};
+console.log("je m'appelle" + " " + moi.prenom + " " + moi.nom + "," + " " + "j'ai" + " " + moi.age + " " + "et j'habite à" + " " + moi.ville)
